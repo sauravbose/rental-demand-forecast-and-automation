1299eea25d3177cff36ad54fed7c32a5c092dd55
sauravbose <saurav.bose93@gmail.com>
2017-06-16 14:50:05 -0400

    Add files via upload

--- database_transfer.py
+++ database_transfer.py
@@ -6,11 +6,13 @@ Created on Thu Jun 15 14:59:47 2017
 """
 
 import pyodbc
+import win32com.client
 import numpy as np
 import pandas as pd
+import sqlite3
 
 #Establish connection with Oracle database
-conn_oracle = pyodbc.connect(DSN = "Database", uid = "", pwd = "")
+conn_oracle = pyodbc.connect(DSN = "Yieldprod", uid = "fanlinli", pwd = "Nino0617")
 cursor = conn_oracle.cursor()
 
 #Retrieve data from Oracle using SQL
@@ -34,9 +36,22 @@ res = pd.DataFrame(res,columns=column_names)
    res = pd.read_sql_query(sql,conn_oracle)''' 
 
 #write the dataframe to MS Excel
-writer = pd.ExcelWriter("H:\oracle_data.xlsx", engine = "xlsxwriter")
-res.to_excel(writer, sheet_name = "Oracle", index = False)
-writer.save()
+#writer = pd.ExcelWriter("H:\oracle_data.xlsx", engine = "xlsxwriter")
+#res.to_excel(writer, sheet_name = "Oracle", index = False)
+#writer.save()
 
-#write the dataframe to MS Access
+#write the dataframe to sqlite database
+conn_sqlit = sqlite3.connect(r"H:\testdata.db")
+cursor_sq = conn_sqlit.cursor()
+
+res.to_sql("First_table",conn_sqlit,if_exists='replace', index=False)
+
+data = pd.read_sql_query("Select * from First_table", conn_sqlit)
+
+print data
+
+conn_sqlit.close()
+
+
+
 
