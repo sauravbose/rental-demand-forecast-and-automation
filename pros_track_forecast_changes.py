238ecfb84be7443a8263a74cbc8dd3108c3fd042
sauravbose <saurav.bose93@gmail.com>
2017-06-26 17:04:43 -0400

    Add files via upload

--- pros_track_forecast_changes.py
+++ pros_track_forecast_changes.py
@@ -11,11 +11,13 @@ import pandas as pd
 import numpy as np
 import calendar
 import datetime
+import win32com.client
+import string
 
 
 def start_connection(databasepath, dsn, oracle, sqlit):
     if oracle:
-        conn_orc = pyodbc.connect(DSN = dsn , uid = "", pwd = "")
+        conn_orc = pyodbc.connect(DSN = dsn , uid = "fanlinli", pwd = "jun2017avis")
         cursor_orc = conn_orc.cursor()
         return [conn_orc,cursor_orc]
     
@@ -28,7 +30,7 @@ def start_connection(databasepath, dsn, oracle, sqlit):
 def update_priorday_fore(conn,forecast_today):
     priorday = pd.read_sql_query("select * from Todays_forecast",conn)
     if(priorday.equals(forecast_today) == False):
-        print 'Executing Update'
+        print 'executing'
         writetosqlite(priorday,"Priordays_forecast",conn)
        
 
@@ -91,9 +93,9 @@ def capture_todays_forecast(conn, conn_sql):
     d = pd.read_sql(sql_finaldata,conn_sql)   
     
     for i in range(len(d)):
-        d.loc[i,"CURR_STRAT_FCST"] = d.loc[i,"CURR_STRAT_FCST"].round(decimals = 5)
-        d.loc[i,"STRAT_7"] = d.loc[i,"STRAT_7"].round(decimals = 5)
-        d.loc[i,"STRAT_1"] = d.loc[i,"STRAT_1"].round(decimals = 5)
+        d.loc[i,"CURR_STRAT_FCST"] = d.loc[i,"CURR_STRAT_FCST"].round(decimals = 0)
+        d.loc[i,"STRAT_7"] = d.loc[i,"STRAT_7"].round(decimals = 0)
+        d.loc[i,"STRAT_1"] = d.loc[i,"STRAT_1"].round(decimals = 0)
 
     start = datetime.datetime.today().strftime("%Y%m%d")
     end = datetime.datetime.strptime(start,"%Y%m%d")
@@ -114,16 +116,17 @@ def capture_todays_forecast(conn, conn_sql):
 
 
 def compute_forecastdiff(conn_sqlit):
-    SQL_forecastdiff = "Select Todays_forecast.COUNTRY, Todays_forecast.REGION, \
-    Todays_forecast.POOL,Todays_forecast.MARKET_GROUP,\
-    Todays_forecast.BRAND,Todays_forecast.FCST_DATE,Todays_forecast.CURR_STRAT_FCST AS\
-    Todays_forecast, Priordays_forecast.CURR_STRAT_FCST AS Priordays_forecast,\
-    Todays_forecast.STRAT_7 AS Todays_STRAT7, Priordays_forecast.STRAT_7 AS Priordays_STRAT7, \
-    Todays_forecast.STRAT_1 AS Todays_STRAT1, Priordays_forecast.STRAT_1 AS Priordays_STRAT1, \
-    SUM(Todays_forecast.CURR_STRAT_FCST - Priordays_forecast.CURR_STRAT_FCST)\
-    AS CURR_Forecastdiff,  SUM(Todays_forecast.STRAT_7 - Priordays_forecast.STRAT_7)\
-    AS STRAT_7_Forecastdiff, SUM(Todays_forecast.STRAT_1 - Priordays_forecast.STRAT_1)\
-    AS STRAT_1_Forecastdiff FROM Priordays_forecast Inner Join Todays_forecast ON\
+    SQL_forecastdiff = "Select Todays_forecast.COUNTRY AS Country, Todays_forecast.REGION AS Region, \
+    Todays_forecast.POOL AS Pool,Todays_forecast.MARKET_GROUP AS Mrkt_Grp,\
+    Todays_forecast.BRAND AS Brand,Todays_forecast.FCST_DATE AS Co_Date, \
+    Todays_forecast.CURR_STRAT_FCST AS Dmnd_Today, \
+    Priordays_forecast.CURR_STRAT_FCST AS Dmnd_Yesterday,\
+    SUM(Todays_forecast.CURR_STRAT_FCST - Priordays_forecast.CURR_STRAT_FCST) AS Total_Change,\
+    Todays_forecast.STRAT_1 AS Dmnd_Today_S1, Priordays_forecast.STRAT_1 AS Dmnd_Yesterday_S1, \
+    SUM(Todays_forecast.STRAT_1 - Priordays_forecast.STRAT_1) AS Total_Change_S1, \
+    Todays_forecast.STRAT_7 AS Dmnd_Today_S7, Priordays_forecast.STRAT_7 AS Dmnd_Yesterday_S7, \
+    SUM(Todays_forecast.STRAT_7 - Priordays_forecast.STRAT_7) AS Total_Change_S7 \
+    FROM Priordays_forecast Inner Join Todays_forecast ON\
     (Priordays_forecast.POOL=Todays_forecast.POOL) AND\
     (Priordays_forecast.MARKET_GROUP=Todays_forecast.MARKET_GROUP) AND\
     (Priordays_forecast.BRAND=Todays_forecast.BRAND) AND \
@@ -138,7 +141,18 @@ def compute_forecastdiff(conn_sqlit):
     
     writetosqlite(fore_diff,"Summary_forecastdiff",conn_sqlit)
     
-    write_data("pros_forecastdiff.xlsx",fore_diff)
+    fore_diff_excel = fore_diff.copy()
+    for i in range(len(fore_diff_excel)):
+        fore_diff_excel.loc[i,"Co_Date"] = datetime.datetime.strptime(fore_diff_excel.loc[i,"Co_Date"],"%Y%m%d")
+        fore_diff_excel.loc[i,"Co_Date"] = datetime.datetime.strftime(fore_diff_excel.loc[i,"Co_Date"],"%m/%d/%Y")
+    
+    #write_data("pros_forecastdiff.xlsx","Detailed",fore_diff)
+    write_data(writer,"Detailed",fore_diff_excel)
+    
+    num2alpha = dict(zip(range(1, 27), string.ascii_uppercase))
+    workbook  = writer.book
+    worksheet = writer.sheets["Detailed"]
+    worksheet.autofilter('A1:{0}{1}'.format(num2alpha[len(fore_diff.columns)],len(fore_diff)))
 
 
 def daterange(conn_sqlit):
@@ -147,34 +161,54 @@ def daterange(conn_sqlit):
     rng = pd.date_range(start,end)
     
     dates = pd.DataFrame()
-    dates["date"] = rng
-    dates["date"]= dates["date"].apply(lambda x: x.date())    
-    dates["date"]= dates["date"].apply(lambda x: x.strftime('%Y%m%d'))
+    dates["Date"] = rng
+    dates["Date"]= dates["Date"].apply(lambda x: x.date())    
+    dates["Date"]= dates["Date"].apply(lambda x: x.strftime('%Y%m%d'))
      
-    dates["month"] = pd.DatetimeIndex(dates["date"]).month
-    dates["month"] = dates["month"].apply(lambda x: calendar.month_abbr[x])
+    dates["Month"] = pd.DatetimeIndex(dates["Date"]).month
+    dates["Month"] = dates["Month"].apply(lambda x: calendar.month_abbr[x])
     
-    dates["year"] = pd.DatetimeIndex(dates["date"]).year
+    dates["Year"] = pd.DatetimeIndex(dates["Date"]).year
     
     writetosqlite(dates,"Dates",conn_sqlit)
         
 
 def summarize_forecastdiff(conn_sqlit):
-    SQL_finalforecastchange = "SELECT Summary_forecastdiff.COUNTRY,\
-    Summary_forecastdiff.REGION,Summary_forecastdiff.POOL, Summary_forecastdiff.BRAND, \
-    Dates.month, Dates.year,\
-    Sum(Summary_forecastdiff.Priordays_forecast) AS Curr_Strat_Demand_Yesterday,\
-    Sum(Summary_forecastdiff.Todays_forecast) AS Curr_Strat_Demand_Today,\
-    Sum(Summary_forecastdiff.CURR_Forecastdiff) AS Curr_Strat_TotalChange,\
-    Sum(Summary_forecastdiff.CURR_Forecastdiff)/Sum(Summary_forecastdiff.Priordays_forecast)\
-    AS Curr_Strat_PercentChange FROM Summary_forecastdiff\
-    INNER JOIN Dates ON Summary_forecastdiff.FCST_DATE = Dates.date\
-    GROUP BY Summary_forecastdiff.COUNTRY, Summary_forecastdiff.REGION, \
-    Summary_forecastdiff.POOL, Summary_forecastdiff.BRAND, Dates.month, Dates.year"
+    
+    region_dict = {"CENTRAL REGION":"CENT", "NORTHEAST REGION":"NE", "SOUTHEAST REGION":"SE", "WESTERN REGION":"WEST"}
+    month_dict = {"Jan":"[1]JAN","Feb":"[2]FEB","Mar":"[3]MAR", "Apr":"[4]APR","May":"[5]MAY","Jun":"[6]JUN", "Jul":"[7]JUL", "Aug":"[8]AUG", "Sep":"[9]SEP", "Oct":"[10]OCT", "Nov":"[11]NOV", "Dec":"[12]DEC"}
+    
+    SQL_finalforecastchange = "SELECT Summary_forecastdiff.Country,\
+    Summary_forecastdiff.Region,Summary_forecastdiff.Pool, \
+    Summary_forecastdiff.Brand, Dates.Month, Dates.Year,\
+    Sum(Summary_forecastdiff.Dmnd_Yesterday) AS Dmnd_Yesterday,\
+    Sum(Summary_forecastdiff.Dmnd_Today) AS Dmnd_Today,\
+    Sum(Summary_forecastdiff.Total_Change) AS Total_Change,\
+    Sum(Summary_forecastdiff.Total_Change)/Sum(Summary_forecastdiff.Dmnd_Yesterday)\
+    AS Perc_Change FROM Summary_forecastdiff\
+    INNER JOIN Dates ON Summary_forecastdiff.Co_Date = Dates.Date\
+    GROUP BY Summary_forecastdiff.Country, Summary_forecastdiff.Region, \
+    Summary_forecastdiff.Pool, Summary_forecastdiff.Brand, Dates.Month, Dates.Year"
     
     sum_forecastdiff = pd.read_sql_query(SQL_finalforecastchange,conn_sqlit)
+    
+    for i in range(len(sum_forecastdiff)):
+        sum_forecastdiff.loc[i,"Perc_Change"] = sum_forecastdiff.loc[i,"Perc_Change"].round(decimals = 2)
+   
+   
+    for i in range(len(sum_forecastdiff)):
+        sum_forecastdiff.loc[i,"Region"] = region_dict[sum_forecastdiff.loc[i,"Region"]]
+        sum_forecastdiff.loc[i,"Month"] =  month_dict[sum_forecastdiff.loc[i,"Month"]]
+             
     writetosqlite(sum_forecastdiff,"Final_forecast_change",conn_sqlit)
-    write_data("pros_final_forecast_change.xlsx",sum_forecastdiff)
+    write_data(writer,"Final",sum_forecastdiff)
+    
+    num2alpha = dict(zip(range(1, 27), string.ascii_uppercase))
+    workbook  = writer.book
+    worksheet = writer.sheets["Final"]
+    format2 = workbook.add_format({'num_format': '0%'})
+    worksheet.set_column('J:J', None, format2)
+    worksheet.autofilter('A1:{0}{1}'.format(num2alpha[len(sum_forecastdiff.columns)],len(sum_forecastdiff)))
 
 def close_connection(conn):
     conn.close()
@@ -185,19 +219,33 @@ def writetosqlite(data,tablename,conn):
     
     
 
-def write_data(filename,data):
-    writer = pd.ExcelWriter(filename,engine = 'xlsxwriter')
-    data.to_excel(writer,sheet_name = "Sheet1", index = False)
-    writer.save()
+def write_data(writer,sheetname,data):
+    
+    data.to_excel(writer,sheet_name = sheetname, index = False)
+    
 
+def format_excel():
+    excel = win32.gencache.EnsureDispatch('Excel.Application')
+    wb = excel.Workbooks.Open(r'H:\pros_forecast_change_tracking.xlsx')
+    ws1 = wb.Worksheets("Final")
+    ws1.Columns.AutoFit()
+    ws2 = wb.Worksheets("Detailed")
+    ws2.Columns.AutoFit()
+    wb.Save()
+    excel.Application.Quit()
+    
+    
+    
 if __name__=="__main__":
+    
+    writer = pd.ExcelWriter("pros_forecast_change_tracking.xlsx",engine = 'xlsxwriter')
     
     [conn_orc,cursor_orc] = start_connection(None, "ProsUAT", oracle = True, sqlit = False)
     [conn_sqlit,cursor_sqlit] = start_connection(r"H:\pros_forecast_tracking.db", "ProsUAT", oracle = False, sqlit = True)
     
     #Capture today's forecast
     forecast_today = capture_todays_forecast(conn_orc,conn_sqlit)
-    write_data("pros_todaysforecast.xlsx", forecast_today)
+    #write_data(writer, "pros_today",forecast_today)
     close_connection(conn_orc)
     
            
@@ -222,4 +270,8 @@ if __name__=="__main__":
     summarize_forecastdiff(conn_sqlit)
     
     close_connection(conn_sqlit)
+    writer.save()
+    
+    format_excel()
+    
     
